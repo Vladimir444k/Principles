ec9d4570fba15709cffd6c63ff7163b8c2fb0b6d
Vladimir444k <gluschenkovladimir337@gmail.com>
2023-05-26 10:42:52 +0300

    principles commit

---
+++ src/main/java/ru/netology/principles/Post.java
@@ -0,0 +1,67 @@
+package ru.netology.principles;
+
+public class Post {
+    public String name;
+    public String passport;
+    public String patronymic;
+    public String phone;
+    public String surname;
+    public String subscription;
+    public FormDate birthday;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPassport() {
+        return passport;
+    }
+
+    public  void setPassport(String passport) {
+        this.passport = passport;
+    }
+
+    public String getPatronymic() {
+        return patronymic;
+    }
+
+    public void setPatronymic(String patronymic) {
+        this.patronymic = patronymic;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
+
+    public String getSubscription() {
+        return subscription;
+    }
+
+    public void setSubscription(String subscription) {
+        this.subscription = subscription;
+    }
+
+    public FormDate getBirthday() {
+        return birthday;
+    }
+
+    public void setBirthday(FormDate birthday) {
+        this.birthday = birthday;
+    }
+}
